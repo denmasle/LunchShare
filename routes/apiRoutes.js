72a761a2c7fd159a9398211b57ea2b4d850a5a1d
Joseph <joseph@ahnga.net>
2019-03-09 04:55:11 -0800

    Completed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -30,21 +30,21 @@ app.delete("/user/:id", function (req, res) {
   db.Lunch.destroy({
     where: { id: req.params.id }
   })
-  .then(function (dbLunches) {
-    res.json(dbLunches);
-  }).catch(function (err) {
-    throw err;
-  });
+    .then(function (dbLunches) {
+      res.json(dbLunches);
+    }).catch(function (err) {
+      throw err;
+    });
 });
 
 //All tradable lunches
 app.get("/lunch", function (req, res) {
   db.Lunch.findAll({})
-  .then(function (dbLunches) {
-    res.json(dbLunches);
-  }).catch(function (err) {
-    throw err;
-  });
+    .then(function (dbLunches) {
+      res.json(dbLunches);
+    }).catch(function (err) {
+      throw err;
+    });
 });
 
 //Search lunches
@@ -52,11 +52,37 @@ app.get("/lunch/:food", function (req, res) {
   db.Lunch.findAll({
     where: { lunch: req.params.food }
   })
-  .then(function (dbLunches) {
-    res.json(dbLunches);
-  }).catch(function (err) {
-    throw err;
-  });
+    .then(function (dbLunches) {
+      res.json(dbLunches);
+    }).catch(function (err) {
+      throw err;
+    });
+});
+
+app.put("/lunch", function (req, res) {
+  db.Lunch.update(
+    req.body,
+    {
+      where: {
+        id: req.body.id
+      }
+    }).then(function (dbLunches) {
+      res.json(dbLunches);
+    }).catch(function (err) {
+      throw err;;
+    });
+});
+
+//Get food
+app.get("/food/:id", function (req, res) {
+  db.Lunch.findOne({
+    where: { id: req.params.id }
+  })
+    .then(function (dbLunches) {
+      res.json(dbLunches);
+    }).catch(function (err) {
+      throw err;
+    });
 });
 
 module.exports = app;
